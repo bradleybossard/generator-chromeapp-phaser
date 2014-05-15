9cfde3167a0f6728b8ea13b5efaeb89872437dc0
Bradley Bossard <bradleybossard@gmail.com>
2014-05-14 18:25:54 -0700

    Added logic for adding game states to Phaser

--- app/templates/scripts/index.js
+++ app/templates/scripts/index.js
@@ -1,7 +1,11 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
-    var h1 = document.getElementsByTagName('h1');
-    if (h1.length > 0) {
-        h1[0].innerText = h1[0].innerText + ' \'Allo';
-    }
+  var game = new Phaser.Game(<%= gameWidth %>, <%= gameHeight %>, Phaser.AUTO, '<%= _.slugify(appName) %>');
+
+  // Game States
+  <% _.forEach(gameStates, function(gameState) {  %>game.state.add('<%= gameState.shortName %>', require('./states/<%= gameState.shortName %>'));
+  <% }); %>
+
+  game.state.start('boot');
+
 }, false);
