59487f2d6398a04a451f3c6b1561667704a0cb86
Bradley Bossard <bradleybossard@gmail.com>
2014-05-15 15:29:09 -0700

    Changed logic for adding states to the games, got rid of requireJS.  Added /* global */ comments for jshint

--- app/templates/scripts/index.js
+++ app/templates/scripts/index.js
@@ -1,11 +1,16 @@
+/* global Phaser */
+<% _.forEach(gameStates, function(gameState) {  %>/* global <%= gameState.stateName %> */
+<% }); %>
+
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
-  var game = new Phaser.Game(<%= gameWidth %>, <%= gameHeight %>, Phaser.AUTO, '<%= _.slugify(appName) %>');
+    var game = new Phaser.Game(<%= gameWidth %>, <%= gameHeight %>, Phaser.AUTO, '<%= _.slugify(appName) %>');
 
-  // Game States
-  <% _.forEach(gameStates, function(gameState) {  %>game.state.add('<%= gameState.shortName %>', require('./states/<%= gameState.shortName %>'));
-  <% }); %>
+    // Game States
 
-  game.state.start('boot');
+    <% _.forEach(gameStates, function(gameState) {  %>game.state.add('<%= gameState.shortName %>', <%= gameState.stateName %>);
+    <% }); %>
+
+    game.state.start('boot');
 
 }, false);
